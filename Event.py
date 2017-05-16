e8101a31ab23efe5632ad92c937f5843c63ac14b
dummy11 <lilinyue20050163@163.com>
2017-05-16 17:43:37 +0800

    added by Adam Lee

--- Event.py
+++ Event.py
@@ -54,7 +54,8 @@ class MainWindow(wx.Frame):
     def OnOpen(self,e):
         """ Open a file"""
         self.dirname = ''
-        dlg = wx.FileDialog(self, "Choose a file", self.dirname, "", "*.*", wx.OPEN)
+        #dlg = wx.FileDialog(self, "Choose a file", self.dirname, "", "*.*", wx.OPEN)
+        dlg = wx.FileDialog(self, "Choose a file", self.dirname, "", "*.*")
         if dlg.ShowModal() == wx.ID_OK:
             self.filename = dlg.GetFilename()
             self.dirname = dlg.GetDirectory()
