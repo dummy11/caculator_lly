087139c6dc1844020f9342e432e4a55b33dc7b8c
dummy11 <lilinyue20050163@163.com>
2017-05-16 11:12:01 +0800

    added by Adam Lee

---
+++ mainWindow.py
@@ -0,0 +1,38 @@
+#!/user/bin/env python
+# encoding: utf-8
+
+"""
+@Version: python2.7
+@Author: 'linyue.li'
+@License: personality reserved
+@Site: 'www.dummy11.cm'
+@File_name: .py
+@Time : 2017/05/05 15:20
+@Discription: ...
+"""
+
+import wx
+
+class MainWindow(wx.Frame):
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(200,100))
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.CreateStatusBar() # A Statusbar in the bottom of the window
+
+        # Setting up the menu.
+        filemenu= wx.Menu()
+
+        # wx.ID_ABOUT and wx.ID_EXIT are standard IDs provided by wxWidgets.
+        filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
+        filemenu.AppendSeparator()
+        filemenu.Append(wx.ID_EXIT,"E&xit"," Terminate the program")
+
+        # Creating the menubar.
+        menuBar = wx.MenuBar()
+        menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
+        self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
+        self.Show(True)
+
+app = wx.App(False)
+frame = MainWindow(None, "Sample editor")
+app.MainLoop()
