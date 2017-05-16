e7fa7226d2fb7a3166049cff32f89c148d4e2355
dummy11 <lilinyue20050163@163.com>
2017-05-16 14:06:59 +0800

    added by Adam Lee

---
+++ caculator.py
@@ -0,0 +1,19 @@
+#!/user/bin/env python
+# encoding: utf-8
+
+"""
+@Version: python2.7
+@Author: 'linyue.li'
+@License: personality reserved
+@Site: 'www.dummy11.cm'
+@File_name: .py
+@Time : 2017/05/05 15:20
+@Discription: ...
+"""
+
+import wx
+
+app = wx.app()
+frame = wx.frame(None, -1, Caculator)
+frame.show
+app.MainLoop()
