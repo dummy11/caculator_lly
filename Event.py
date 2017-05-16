eeed43574fde04efea78b73e95ce9d6f61ccc6a1
dummy11 <lilinyue20050163@163.com>
2017-05-16 15:34:44 +0800

    added by Adam Lee

---
+++ Event.py
@@ -0,0 +1,68 @@
+#!/user/bin/env python
+# encoding: utf-8
+
+"""
+@Version: python2.7
+@Author: 'linyue.li'
+@License: personality reserved
+@Site: 'www.dummy11.cm'
+@File_name: .py
+@Time : 2017/05/05 15:20
+@Discription: ...
+"""
+
+import os
+import wx
+
+
+class MainWindow(wx.Frame):
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(200, 100))
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        # A StatusBar in the bottom of the window
+        self.CreateStatusBar()
+
+        # Setting up the menu.
+        filemenu= wx.Menu()
+
+        # wx.ID_ABOUT and wx.ID_EXIT are standard ids provided by wxWidgets.
+        menuAbout = filemenu.Append(wx.ID_ABOUT, "&About", " Information about this program")
+        menuExit = filemenu.Append(wx.ID_EXIT, "E&xit", " Terminate the program")
+        menuOpen= filemenu.Append(wx.ID_OPEN, "&Open", " Open a file to edit")
+
+        # Creating the menubar.
+        menuBar = wx.MenuBar()
+        menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
+        self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
+
+        # Set events.
+        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
+        self.Bind(wx.EVT_MENU, self.OnOpen, menuOpen)
+
+        self.Show(True)
+
+    def OnAbout(self,e):
+        # A message dialog box with an OK button. wx.OK is a standard ID in wxWidgets.
+        dlg = wx.MessageDialog( self, "A small text editor", "About Sample Editor", wx.OK)
+        dlg.ShowModal() # Show it
+        dlg.Destroy() # finally destroy it when finished.
+
+    def OnExit(self,e):
+        self.Close(True)  # Close the frame.
+
+    def OnOpen(self,e):
+        """ Open a file"""
+        self.dirname = ''
+        dlg = wx.FileDialog(self, "Choose a file", self.dirname, "", "*.*", wx.OPEN)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.filename = dlg.GetFilename()
+            self.dirname = dlg.GetDirectory()
+            f = open(os.path.join(self.dirname, self.filename), 'r')
+            self.control.SetValue(f.read())
+            f.close()
+        dlg.Destroy()
+
+app = wx.App(False)
+frame = MainWindow(None, "Sample editor")
+app.MainLoop()
